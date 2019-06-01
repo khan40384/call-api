13650d90b5f09f13172adcd73e7a1571ac35637c
khan40384 <khan40384@gmail.com>
2019-06-01 18:09:05 +0530

    f

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 
 http.createServer((req,res) =>{
 	
-	if(req.url=== "/v3/user/list?clientId=4e37a73f60064861a7b107b776b23fae&clientSecret=422c5e305e671499f1329c7be9c5ef28&startDate=0&endDate=0&pageNo=1&pageSize=10&date=Date/request"){
+	/*if(req.url=== "/v3/user/list?clientId=4e37a73f60064861a7b107b776b23fae&clientSecret=422c5e305e671499f1329c7be9c5ef28&startDate=0&endDate=0&pageNo=1&pageSize=10&date=Date/request"){
 		apiCallFromRequest.callApi(function(response){
 			console.log(JSON.stringify(response));
 			res.write(JSON.stringify(response));
@@ -20,8 +20,8 @@ http.createServer((req,res) =>{
 			res.write(response);
             res.end();
 		});
-	}
-
+	}*/
+	
    const callApi = () =>{
 	https.get(`https://api.sciener.cn${req.url}`,(resp)=>{
 		let data='';
