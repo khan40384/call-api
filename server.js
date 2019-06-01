6185919d6e74dc6211d0066f2d743cfe99ab5c04
khan40384 <khan40384@gmail.com>
2019-06-01 17:21:36 +0530

    last commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 http.createServer((req,res) =>{
-	if(req.url=== "/request"){
+	if(req.url=== "/v3/user/list?clientId=4e37a73f60064861a7b107b776b23fae&clientSecret=422c5e305e671499f1329c7be9c5ef28&startDate=0&endDate=0&pageNo=1&pageSize=10&date=Date/request"){
 		apiCallFromRequest.callApi(function(response){
 			console.log(JSON.stringify(response));
 			res.write(JSON.stringify(response));
