0c27f71929988239f658a94dd5acae6558ceb303
khan40384 <khan40384@gmail.com>
2019-06-01 17:58:39 +0530

    D

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 http.createServer((req,res) =>{
+	
 	if(req.url=== "/v3/user/list?clientId=4e37a73f60064861a7b107b776b23fae&clientSecret=422c5e305e671499f1329c7be9c5ef28&startDate=0&endDate=0&pageNo=1&pageSize=10&date=Date/request"){
 		apiCallFromRequest.callApi(function(response){
 			console.log(JSON.stringify(response));
@@ -14,12 +15,29 @@ http.createServer((req,res) =>{
 			res.end();
 		});
 	}
-	else if(req.url === '/v3/user/list?clientId=4e37a73f60064861a7b107b776b23fae&clientSecret=422c5e305e671499f1329c7be9c5ef28&startDate=0&endDate=0&pageNo=1&pageSize=10&date=Date'){
+	else if(req.url === '/v3/user/list?clientId=4e37a73f60064861a7b107b776b23fae&clientSecret=422c5e305e671499f1329c7be9c5ef28&startDate=0&endDate=0&pageNo=1&pageSize=10&date=Date/node'){
 		apiCallFromNode.callApi(function(response){
 			res.write(response);
             res.end();
 		});
 	}
+
+   function(){
+	https.get(`https://api.sciener.cn${req.url}`,(resp)=>{
+		let data='';
+		resp.on('data',(chunk)=>{
+			data += chunk;
+			console.log(data);
+			res.write(data)
+		});
+		resp.on('end',()=>{
+         callback(data);
+		});
+	}).on("error",(err)=>{
+		console.log("error:" + err.message);
+	});
+}
+
 }).listen(port);
 
 
