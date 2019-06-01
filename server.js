0d5abebbe3f633d5749bfa345f174a133c71e236
khan40384 <khan40384@gmail.com>
2019-06-01 16:54:26 +0530

    DF

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ http.createServer((req,res) =>{
 			res.end();
 		});
 	}
-	else if(req.url === '/'){
+	else if(req.url === '/v3/user/list?clientId=4e37a73f60064861a7b107b776b23fae&clientSecret=422c5e305e671499f1329c7be9c5ef28&startDate=0&endDate=0&pageNo=1&pageSize=10&date=Date'){
 		apiCallFromNode.callApi(function(response){
 			res.write(response);
             res.end();
