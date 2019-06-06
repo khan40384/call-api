8ce7e757d95c3f07062552dfa1ae2b89e76877d6
khan40384 <khan40384@gmail.com>
2019-06-06 14:53:11 +0530

    handling post request

--- server.js
+++ server.js
@@ -1,15 +1,47 @@
 
-const http = require('http');
-const https = require('https');
+
+const htt = require('http');
+const htts = require('https');
+htts.post = require('https-post');
 
 const port = process.env.PORT || 3000;
 
-http.createServer((req,res) =>{
+htt.createServer('request',(req,res) =>{
+	console.log(req.url);
+
 	
+
+var params=function(req){
+  let q=req.url.split('?'),result={};
+  if(q.length>=2){
+      q[1].split('&').forEach((item)=>{
+           try {
+             result[item.split('=')[0]]=item.split('=')[1];
+           } catch (e) {
+             result[item.split('=')[0]]='';
+           }
+      })
+  }
+  return result;
+}
+
+req.params=params(req);
+
+console.log(req.params.username);
+console.log(req.params.password);
+  if(req.method==='POST'){
 	
-   
-	https.get(`https://api.sciener.cn${req.url}`,(resp)=>{
-		let data='';
+	htts.post(`https://api.sciener.cn${req.url}`,{
+		client_id: req.params.username,
+		client_secret: req.params.client_secret,
+		grant_type: req.params.grant_type,
+
+		username: req.params.username, 
+		password: req.params.password,
+		redirect_uri: req.params.redirect_uri
+	}, function(resp){
+	resp.setEncoding('utf8');
+	let data='';
 		resp.on('data',(chunk)=>{
 			data += chunk;
 			console.log(data);
@@ -24,11 +56,43 @@ http.createServer((req,res) =>{
 		res.write(err.message);
 		res.end();
 	});
+	
+    }
+    if(req.method==='GET')
+    {
+	htts.get(`https://api.sciener.cn${req.url}`,(resp)=>{
+		let data='';
+		resp.on('data',(chunk)=>{
+			data += chunk;
+			console.log(data);
+			res.write(data);
+			res.end();
+		});
+		resp.on('end',()=>{
+        
+		});
+	}).on("error",(err)=>{
+		console.log("error:" + err.message);
+		res.write(err.message);
+		res.end();
 
+	});
+
+	
+
+
+
+}
 
 }).listen(port);
 
 
 
 console.log(`Listening on port ${port}...`);
+
+
+
+
+
+
 
