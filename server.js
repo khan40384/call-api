04eb66a0a5d9376720adbcb4101109d3bcb60320
khan40384 <khan40384@gmail.com>
2019-06-07 12:41:59 +0530

    DS

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 
-const htt = require('http');
+/*const htt = require('http');
 const htts = require('https');
 htts.post = require('https-post');
 
@@ -51,7 +51,6 @@ console.log(req.params.redirect_uri);
 		clientId: req.params.client_id,
 		clientSecret: req.params.client_secret,
 		grantType: req.params.grant_type,
-
 		username: req.params.username, 
 		password: req.params.password,
 		redirect_uri: req.params.redirect_uri
@@ -105,6 +104,87 @@ console.log(req.params.redirect_uri);
 
 
 console.log(`Listening on port ${port}...`);
+
+*/
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const https = require('https');
+https.post = require('https-post');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+app.get('/v3/user/list', (req,res) => {
+	console.log(req.url);
+	console.log(req.params);
+	https.get(`https://api.sciener.cn${req.url}`,(resp)=>{
+		let data='';
+		resp.on('data',(chunk)=>{
+			data += chunk;
+			console.log(data);
+			res.write(data);
+			res.end();
+		});
+		resp.on('end',()=>{
+        
+		});
+	}).on("error",(err)=>{
+		console.log("error:" + err.message);
+		res.write(err.message);
+		res.end();
+});
+})
+
+
+app.post('/oauth2/token',(req,res) => {
+	console.log(req.query);
+	console.log(req.params);
+	console.log(req.query.username);
+console.log(req.query.grant_type);
+console.log(req.query.password);
+console.log(req.query.client_id);
+console.log(req.query.client_secret);
+console.log(req.query.redirect_uri);
+     https.post(`https://api.sciener.cn${req.url}`,{
+		client_id: req.query.client_id,
+		client_secret: req.query.client_secret,
+		grant_type: req.query.grant_type,
+		username: req.query.username, 
+		password: req.query.password,
+		redirect_uri: req.query.redirect_uri
+	}, function(resp){
+	resp.setEncoding('utf8');
+	let data='';
+		resp.on('data',(chunk)=>{
+			data += chunk;
+			console.log(data);
+			res.write(data);
+			res.end();
+		});
+		resp.on('end',()=>{
+        
+		});
+	}).on("error",(err)=>{
+		console.log("error:" + err.message);
+		res.write(err.message);
+		res.end();
+	});
+
+})
+
+
+
+
+app.listen(3000||process.env.PORT, ()=>{
+	console.log(`app is running at port ${process.env.PORT}`);
+})
 
 
 
